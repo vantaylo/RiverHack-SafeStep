2164bc0f72239746b54733c43a7bab5306c5aef7
Luis Quinones <thelfq@gmail.com>
2025-04-26 17:55:21 -0500

    import location from other file

--- SafeStep/app/index.tsx
+++ SafeStep/app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import {location} from "./map";
 
 export default function Index() {
   return (
@@ -9,7 +10,8 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Edit app/index.tsx to edit this screen.{location}</Text>
+      
     </View>
   );
 }
