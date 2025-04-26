40f8d929e14b0775798374cd824cc8ab64364fe8
Josh <152348285+ExtraMoopy@users.noreply.github.com>
2025-04-26 22:13:47 +0000

    Added colors to text, changed background color, and font spacing.

--- SafeStep/app/index.tsx
+++ SafeStep/app/index.tsx
@@ -7,9 +7,23 @@ export default function Index() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: "#3733ff",
       }}
     >
-      <Text>TITLE</Text>
+      <Text
+        style={{
+          fontSize: 30, // Larger text size
+          fontFamily: "Helvetica", // System font
+          fontWeight: "bold", // Bold text
+          color: "white", // Purple text
+          textAlign: "center", // Center the text horizontally
+          textTransform: "uppercase", // Uppercase text
+          letterSpacing: 4, // Spacing between letters
+          marginTop: -255,
+        }}
+      >
+        SafeStep
+      </Text>
     </View>
   );
 }
