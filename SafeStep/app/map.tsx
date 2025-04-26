6aee5cd8caaa233691fb9cfcea09e432e6e719db
Luis Quinones <thelfq@gmail.com>
2025-04-26 17:56:29 -0500

    nonfunctioning location getter code

--- SafeStep/app/map.tsx
+++ SafeStep/app/map.tsx
@@ -1,53 +1,57 @@
 import React, { useEffect, useState } from 'react';
-import { View, StyleSheet } from "react-native";
-import MapView, {Marker} from 'react-native-maps';
+import {Text, View, StyleSheet } from "react-native";
+;import MapView, {Marker} from 'react-native-maps';
 import Geolocation from 'react-native-geolocation-service';
 
-const MyMap = () => {
-  const [location, setLocation] = useState(null);
 
-  useEffect(() => {
-    // Request the user's location
-    Geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
-        setLocation({
-          latitude,
-          longitude,
-          latitudeDelta: 0.01, // Adjust zoom level
-          longitudeDelta: 0.01,
-        });
-      },
-      (error) => {
-        console.error('Error getting location: ', error);
-      },
-      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
-    );
-  }, []);
 
-  return (
-    <View style={map_styles.container}>
-      {location ? (
-        <MapView
-          style={map_styles.map}
-          initialRegion={location}
-        >
-          {/* Add a marker at the user's location */}
-          <Marker coordinate={location} />
-        </MapView>
-      ) : null}
-    </View>
-  );
-};
+Geolocation.getCurrentPosition(
+    (position) => {
+      console.log(position);
+    },
+    (error) => {
+      // See error code charts below.
+      console.log(error.code, error.message);
+    },
+    { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+);
 
-export default MyMap;
+// const MyMap = () => {
+//   const [location, setLocation] = useState(null);
 
-const map_styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  map: {
-    width: '100%',
-    height: '100%',
-  },
-});
+//   useEffect(() => {
+//     if (hasLocationPermission)
+//     // Request the user's location
+//     Geolocation.getCurrentPosition(
+//       (position) => {
+//         const { latitude, longitude } = position.coords;
+//         setLocation({
+//           latitude,
+//           longitude,
+//           latitudeDelta: 0.01, // Adjust zoom level
+//           longitudeDelta: 0.01,
+//         });
+//       },
+//       (error) => {
+//         console.error('Error getting location: ', error);
+//       },
+//       { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+//     );
+//   }, []);
+
+//   return (
+//      location
+//   );
+// };
+
+// export default location;
+
+// const map_styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//   },
+//   map: {
+//     width: '100%',
+//     height: '100%',
+//   },
+// });
