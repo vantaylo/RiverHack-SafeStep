12cb2c2f770983acc5430aa19abf668ead8cf8b5
xenoxndr <156947918+xenoxndr@users.noreply.github.com>
2025-04-27 10:32:28 -0500

    isolated getStatus

---
+++ SafeStep/app/getStatus.tsx
@@ -0,0 +1,34 @@
+import { signalGroupObject } from "./Components/getIntersection";
+//===========================================
+// Config
+//===========================================
+//Minimum time for crossing
+const crossingTime:number = 30;
+//===========================================
+// Get Status Code and helper objects
+//===========================================
+enum LightStatus{
+    Green = "Green",
+    Yellow= "Yellow",
+    Red   = "Red"
+}
+
+export enum Status{
+  Walk="Walk",
+  Warning="Warning",
+  Stop="STOP"
+}
+
+export const getStatus = (data: signalGroupObject) =>{
+  let status:Status;
+  if(data.signalPhase === LightStatus.Green && data.timeRemaining >= crossingTime){
+    status= Status.Walk;
+  }
+  else if (data.signalPhase === LightStatus.Red) {
+    status= Status.Stop;
+  } 
+  else {
+    status= Status.Warning;    
+  }
+ return status;
+};
