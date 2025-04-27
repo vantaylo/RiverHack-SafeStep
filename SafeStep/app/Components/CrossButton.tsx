8cfd7039050bf1305d89446d592173b110c2633f
vantaylo <vanessalyn.taylor@gmail.com>
2025-04-27 11:46:22 -0500

    uninstall mapgep libs, btn styling, utils folder

--- SafeStep/app/Components/CrossButton.tsx
+++ SafeStep/app/Components/CrossButton.tsx
@@ -1,20 +1,21 @@
-import { Button, TouchableOpacity } from "react-native";
+import { TouchableOpacity, Text } from "react-native";
 import { useState } from "react";
 
 export default function CrossButton({ onPress }: { onPress: () => void; }) {
   const [crossBtn, setCrossBtn] = useState(true);
 
   return (
-    <TouchableOpacity
-      style={{ backgroundColor: '#000000', borderRadius: 5, padding: 10 }}
-      onPress={onPress}
-      accessibilityLabel="PUSH TO CROSS"
-    >
-      <Button
-        color="#FFFFFF"
+      <TouchableOpacity
+        accessibilityLabel="PUSH TO CROSS"
+        style={{
+          backgroundColor: "#000000",
+          padding: 10,
+          borderRadius: 5,
+          alignItems: "center",
+        }}
         onPress={onPress}
-        title="PUSH TO CROSS"
-      />
-    </TouchableOpacity>
+      >
+        <Text style={{ color: "#FFFFFF", fontWeight: "bold" }}>PUSH TO CROSS</Text>
+      </TouchableOpacity>
   );
 }
