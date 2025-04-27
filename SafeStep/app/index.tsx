f204e621743c7d49179e6ef488f81e2da3f9a405
Josh <152348285+ExtraMoopy@users.noreply.github.com>
2025-04-27 00:38:53 +0000

    Removed Images attempt.

--- SafeStep/app/index.tsx
+++ SafeStep/app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
 
 export default function Index() {
   return (
@@ -7,11 +7,27 @@ export default function Index() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        position: "relative",
         backgroundColor: "#3733ff",
       }}
     >
+      <Image
+        source={{
+          uri: "/project/workspace/SafeStep/assets/images/pedestrian_walking_blind.png",
+        }}
+        style={{
+          width: 200,
+          height: 200,
+          borderRadius: 10,
+          resizeMode: "cover",
+        }}
+      />
+
       <Text
         style={{
+          position: "absolute",
+          top: 15,
+          left: 5,
           fontSize: 30, // Larger text size
           fontFamily: "Helvetica", // System font
           fontWeight: "bold", // Bold text
@@ -19,7 +35,6 @@ export default function Index() {
           textAlign: "center", // Center the text horizontally
           textTransform: "uppercase", // Uppercase text
           letterSpacing: 4, // Spacing between letters
-          marginTop: -370,
         }}
       >
         SafeStep
