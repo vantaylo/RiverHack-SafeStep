27dd887775b033806149ec38ee21d1fcbe339795
xenoxndr <156947918+xenoxndr@users.noreply.github.com>
2025-04-27 10:02:07 -0500

    Started function, need to finish defining customVibration

---
+++ SafeStep/app/Components/timer.tsx
@@ -0,0 +1,12 @@
+import {Vibration} from 'react-native';
+
+export default function crossVibration (timeRemaining: number): void {
+    function customVibrate(pauseDuration: number, duration: number): void {
+
+    }
+    // Vibrate 2x to signal safe to cross
+    // Vibrate slow pulse to signal crossing time
+    // Vibrate agressivelly to signal warning
+    // Vibrate pulse to say crossing is done
+}
+
